523d7999f184d88dba430a5a95471eb2c96831ed
王少雄 <14906362+Jacob_rest@user.noreply.gitee.com>
2026-03-05 08:12:56 +0800

    Init

---
+++ src/main/java/com/cloume/rest/app/medicalimagedatamanagementplatform/MedicalImageDataManagementPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.cloume.rest.app.medicalimagedatamanagementplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedicalImageDataManagementPlatformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MedicalImageDataManagementPlatformApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cloume/rest/app/medicalimagedatamanagementplatform/MedicalImageDataManagementPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cloume.rest.app.medicalimagedatamanagementplatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedicalImageDataManagementPlatformApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
